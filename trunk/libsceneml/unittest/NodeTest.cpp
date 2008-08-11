62e05e210dda853f0d59b4363e2e5414749849e2
yamokosk <yamokosk@029fdfca-6e42-0410-8aa1-a10bf05ef713>
2008-08-11 18:33:40 +0000

    Beginning serious effort to unittest all classes.

---
+++ trunk/libsceneml/unittest/NodeTest.cpp
@@ -0,0 +1,32 @@
+/*
+ * NodeTest.cpp
+ *
+ *  Created on: Aug 11, 2008
+ *      Author: yamokosk
+ */
+
+#include <cppunit/config/SourcePrefix.h>
+#include "NodeTest.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION( NodeTest );
+
+void NodeTest::setUp()
+{
+
+}
+
+void NodeTest::tearDown()
+{
+
+}
+
+// Test cases
+void NodeTest::testEquality()
+{
+
+}
+
+void NodeTest::testAddition()
+{
+
+}
---
+++ trunk/libsceneml/unittest/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Aug 11, 2008
+ *      Author: yamokosk
+ */
+#include <cppunit/extensions/HelperMacros.h>
+
+CPPUNIT_TEST_SUITE_REGISTRATION( Node );
+
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+int main( int argc, char **argv)
+{
+	CppUnit::TextUi::TestRunner runner;
+	CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+	runner.addTest( registry.makeTest() );
+	bool wasSucessful = runner.run( "", false );
+	return wasSucessful;
+}
